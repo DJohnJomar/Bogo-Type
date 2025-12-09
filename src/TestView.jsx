0e22655c54b6b6f3c645cd7bff8be09c0e0ecaa3
John Jomar Dimaunahan <johndjomar@gmail.com>
2025-12-09 20:01:36 +0800

    feat: Use API to generate words, controlled with word count

--- src/TestView.jsx
+++ src/TestView.jsx
@@ -1,7 +1,7 @@
 import "./assets/styles/TestView.css";
-import { useState, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 /*
-Utilizing:
+Input focus system:
 React useRef to give focus to the input field
 tabIndex to make input field to be first when tabbing
 
@@ -9,16 +9,32 @@ tabIndex to make input field to be first when tabbing
 */
 function TestView({ pressedKey }) {
   const [typedCharacters, setTypedCharacters] = useState("");
+  const [words, setWords] = useState("");
   const inputRef = useRef(null);
 
-  let words =
-    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut ex non qui ipsam quibusdam facilis nostrum architecto nihil consequatur maiores, unde optio magnam voluptatibus aspernatur asperiores praesentium iste distinctio vero!";
+  async function getWords(wordCount) {
+    let res = await fetch(
+      `https://random-word-api.vercel.app/api?words=${wordCount}`
+    );
+    let data = await res.json();
+    let words = "";
+    data.forEach((word) => {
+      words += `${word} `;
+    });
+    setWords(words);
+  }
+
+  useEffect(() => {
+    getWords(20);
+  }, []);
 
   return (
-    <div
-      className="TestView"
-      onClick={() => inputRef.current.focus()} // ✅ CLICK ANYWHERE TO FOCUS
-    >
+    // onClick, uses useRef to give focus to the element. Click anywhere in the TestView Component to give focus
+    /* Maps per character of words variable
+        if the value of typedCharacter in current index is null, color is black.
+        if not null, if typedCharacter in index is equal to current word character, color is green else red
+      */
+    <div className="TestView" onClick={() => inputRef.current.focus()}>
       <p className="words">
         {[...words].map((char, i) => {
           const typed = typedCharacters[i];
@@ -36,13 +52,11 @@ function TestView({ pressedKey }) {
 
       <div className="cursor"></div>
 
+      {/* The focus will be in the input since it has the useRef object. */}
       <div id="focus-error">
-        <label htmlFor="type-input">
-          Click here/ Press Tab to focus
-        </label>
-
+        <label htmlFor="type-input">Click here/ Press Tab to focus</label>
         <input
-          ref={inputRef}        
+          ref={inputRef}
           tabIndex={0}
           id="type-input"
           type="text"
@@ -51,6 +65,7 @@ function TestView({ pressedKey }) {
           onChange={(e) => setTypedCharacters(e.target.value)}
         />
       </div>
+      {/* <button onClick={() => getWords(200)}>New Test</button> */}
     </div>
   );
 }
