bedb7e4e264c545f2276c2708a7b2be810613813
John Jomar Dimaunahan <johndjomar@gmail.com>
2025-12-27 15:55:07 +0800

    feat: Install react-router and test setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import "./App.css";
 import Keyboard from "./components/Keyboard/Keyboard.jsx";
 import TestView from "./pages/TestView/TestView.jsx";
 import { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-
   const [pressedKey, setPressedKey] = useState(null);
 
   useEffect(() => {
@@ -27,11 +27,13 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>Bogo Type</h1>
-      <TestView/>
-      <Keyboard pressedKey={pressedKey}/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Bogo Type</h1>
+        <TestView />
+        <Keyboard pressedKey={pressedKey} />
+      </div>
+    </BrowserRouter>
   );
 }
 
