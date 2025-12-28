54f448f2c7928f0e8e93092e3419fff2061104b7
John Jomar Dimaunahan <johndjomar@gmail.com>
2025-12-28 17:53:18 +0800

    style: Added some styles to footer github link

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,9 +2,9 @@ import githubIcon from "../../assets/githubIcon.svg";
 
 function Footer(){
     return (
-        <footer className="flex justify-center m-5">
-            <a className="invert flex gap-2  items-center" href="https://github.com/DJohnJomar/Bogo-Type" target="_blank">
-                <img src={githubIcon} alt="GitHub Icon" />
+        <footer className="flex justify-center m-5 ">
+            <a className="invert flex gap-1  items-center hover:border-b-1" href="https://github.com/DJohnJomar/Bogo-Type" target="_blank">
+                <img src={githubIcon} alt="GitHub Icon" className="w-5 h-5"/>
                 <span>GitHub</span>
             </a>
         </footer>
