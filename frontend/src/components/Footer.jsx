8e2ae7e1c98d944779fbf117e0b2de237405a6ef
John Jomar Dimaunahan <johndjomar@gmail.com>
2026-01-03 14:38:37 +0800

    style: Add the hover effect in footer link

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import githubIcon from "../assets/githubIcon.svg";
 function Footer(){
     return (
         <footer className="flex justify-center m-5 ">
-            <a className="invert flex gap-1  items-center hover:border-b-1" href="https://github.com/DJohnJomar/Bogo-Type" target="_blank">
+            <a className="invert flex gap-1  items-center transform transition-transform duration-200 hover:scale-125" href="https://github.com/DJohnJomar/Bogo-Type" target="_blank">
                 <img src={githubIcon} alt="GitHub Icon" className="w-5 h-5"/>
                 <span>GitHub</span>
             </a>
